ca3cba16b473c11766d2ba70d546963cbd3cbfcc
Pranvi <75794298+Pranvi10@users.noreply.github.com>
2021-02-22 14:34:12 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,48 @@
+class Block {
+   constructor(x, y, width, height) {
+        var options={
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false
+        }
+
+         this.Visiblity = 255;
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+
+      }
+
+      display() {
+
+        console.log(this.body.speed);
+ 
+        if(this.body.speed<4){
+          var angle = this.body.angle;
+          push();
+          translate(this.body.position.x, this.body.position.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0, 0, 30, 40);
+          pop();  
+        }
+      
+        else{
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity -3;
+        tint(255, this.Visiblity);
+        //image(this.image, this.body.position.x, this.body.position.y, 50,50);
+        pop();
+        }
+      
+      
+
+            
+         
+
+      }
+      
+}
